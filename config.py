022e17b8a97ba6b79214703f691f24811e2c60f8
jhenao-nex <jaime.andres.henao.arbelaez@ba.com>
2025-04-08 16:58:47 +0000

    feat: fix notifications and rollback:5

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 import os
-from dotenv import load_dotenv
-
-# Cargar las variables desde el archivo .env
-load_dotenv()
 
 class Config:
-    ARGOCD_API = os.getenv("ARGOCD_API", "https://localhost:8080/api/v1")
+    ARGOCD_API = os.getenv("ARGOCD_API", "https://argocd-server.argocd.svc.cluster.local/api/v1")
     ARGOCD_TOKEN = os.getenv("ARGOCD_TOKEN")
-    SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")  # Cargar desde variable de entorno
+    SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
+    GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+    GIT_REPO_URL = os.getenv("GIT_REPO_URL", "https://github.com/JaimeHenaoChallange/monitor-argocd-cronjob-1.git")
+    GIT_BRANCH = os.getenv("GIT_BRANCH", "main")
+    REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", 10))
 
     @staticmethod
     def validate():
-        if not Config.ARGOCD_API or not Config.ARGOCD_TOKEN or not Config.SLACK_WEBHOOK_URL:
-            raise ValueError("❌ Faltan variables de entorno requeridas: ARGOCD_API, ARGOCD_TOKEN o SLACK_WEBHOOK_URL.")
+        required_vars = ["ARGOCD_API", "ARGOCD_TOKEN", "SLACK_WEBHOOK_URL", "GITHUB_TOKEN"]
+        for var in required_vars:
+            if not getattr(Config, var):
+                raise ValueError(f"❌ Faltan variables de entorno requeridas: {var}")
